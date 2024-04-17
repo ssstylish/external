e492dbbff412d7d38a51b7c79c1a864d28e83c84
pollalex <slepehin2005@gmail.com>
2024-04-17 03:48:04 +0300

    Add modules

---
+++ Modules.mk
@@ -0,0 +1,3 @@
+include $(MODULES_PATH)/curl-android/curl/src/main/native/Android.mk
+include $(MODULES_PATH)/zygisk-module-sample/module/jni/libcxx/Android.mk
+include $(MODULES_PATH)/BoringSSL_Android/boringssl/src/main/native/BoringSSL.mk
